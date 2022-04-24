49fb000b1217e4c63d68f1ff497deb7605c26461
Yerendi-hub <konrad.kowalczyk321@gmail.com>
2022-04-24 16:14:06 +0200

    addgame basic logic

--- server/controllers/games.js
+++ server/controllers/games.js
@@ -1,3 +1,24 @@
-export const getGames = (req, res) =>{
-    res.send('test');
+import GameData from "../models/gameData.js";
+
+export const getGames = async (req, res) => {
+    try {
+        const games = await GameData.find();
+        res.status(200).json(games);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const addGame = async (req, res) =>{
+    const body = req.body;
+    
+    const newGame = new GameData(body);
+
+    try {
+        await newGame.save();
+
+        res.status(201).json(newGame);
+    } catch (error) {
+        res.status(409).json({message: error.message});
+    }
 }
